5f935b22ddfb13d0c07b5cc94f0ec2b8930e89a9
RK <r03625524@gmail.com>
2026-09-22 15:55:53 +0530

    Use Render PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const OpenAI = require("openai");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
